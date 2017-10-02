a9469f3638191e43b2e6616c548b2f74fe799ffa
kvakili813 Vakili <kvakili813@gmail.com>
2017-10-02 21:23:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move(board, index)# code your #valid_move? method here
-end  
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
