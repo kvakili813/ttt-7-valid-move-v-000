39c844f955d8678715af867c8e87a15542241170
kvakili813 Vakili <kvakili813@gmail.com>
2017-10-02 22:18:03 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,8 @@
 def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0,8)
+    true
+  else
+    false
 end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
